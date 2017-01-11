9df12297bfe42348365d4e41940e1e3a14819a5a
Justin Jia <justin.jia.developer@gmail.com>
2017-01-10 22:21:07 -0800

    Add a simple launch screen

---
+++ Warcraft2/Warcraft2/LaunchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchViewController.swift
+//  Warcraft2
+//
+//  Created by Justin Jia on 1/10/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+}
