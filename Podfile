3cb61ce9f394a2a7fdcf8427cec808816888a099
Justin Jia <justin.jia.developer@gmail.com>
2017-01-10 23:14:26 -0800

    Add splash screen

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Warcraft2' do
+  pod 'Overlay', '~> 0.6'
+end
---
+++ Warcraft2/UI.swift
@@ -0,0 +1,40 @@
+//
+//  UI.swift
+//  Warcraft2
+//
+//  Created by Justin Jia on 1/10/17.
+//  Copyright © 2017 UC Davis. All rights reserved.
+//
+
+import Overlay
+
+@IBDesignable
+class SplashImageView: UIImageView, CustomBackgroundColor, CustomDesign {
+
+    var backgroundColorStyle: ColorStyle = UIColor.black
+    var design: (DesignCustomizable) -> () = {
+        let imageView = $0 as! UIImageView
+        imageView.contentMode = .scaleAspectFit
+    }
+
+}
+
+@IBDesignable
+class TitleLabel: UILabel, CustomTextColor, CustomFont, CustomTextAlignment {
+
+    var textColorStyle: ColorStyle = UIColor.white
+    var fontStyle: FontStyle = UIFont.preferredFont(forTextStyle: .title1)
+    var textAlignmentStyle: TextAlignmentStyle = NSTextAlignment.center
+
+}
+
+class ActionButton: UIButton, CustomTitleColor, CustomBackgroundColor, CustomDesign {
+
+    var backgroundColorStyle: ColorStyle = UIColor.darkGray
+    var titleColorStyle: ColorStyle = UIColor.white
+    var design: (DesignCustomizable) -> () = {
+        let button = $0 as! UIButton
+        button.layer.cornerRadius = 16
+    }
+
+}
