3ebb29ccb3786728bad19463779470bfdd6078c6
Justin Jia <justin.jia.developer@gmail.com>
2017-01-18 17:04:07 -0800

    Choose option A

--- Warcraft2/Rectangle.swift
+++ Warcraft2/Rectangle.swift
@@ -6,43 +6,16 @@
 //  Copyright © 2017 UC Davis. All rights reserved.
 //
 
-import CoreGraphics
+import Foundation
 
-// Version A:
-//
-// struct Rectangle {
-//     var xPosition = 0
-//     var yPosition = 0
-//     var width = 0
-//     var height = 0
-//
-//     func pointInside(x: Int, y: Int) -> Bool {
-//         return (x >= xPosition) && (x < xPosition + width)
-//             && (y >= yPosition) && (y < yPosition + height)
-//     }
-// }
-//
-// Usuage
-//
-// Rectangle(xPosition: 10, yPosition: 10, width: 100, height: 100).pointInside(x: 80, y: 23)
-
-// Version B:
-//
-// typealias Rectangle = CGRect
-// extension Rectangle {
-//     func pointInside(x: CGFloat, y: CGFloat) -> Bool {
-//         return contains(CGPoint(x: x, y: y))
-//     }
-// }
-//
-// Usuage
-//
-// Rectangle(x: 10, y: 10, width: 100, height: 100).pointInside(x: 80, y: 23)
-
-// Version C:
-//
-// Use `CGRect` directly
-//
-// Example:
-//
-// CGRect(x: 10, y: 10, width: 100, height: 100).contains(CGPoint(x: 80, y: 23))
+struct Rectangle {
+    var xPosition = 0
+    var yPosition = 0
+    var width = 0
+    var height = 0
+    
+    func contains(x: Int, y: Int) -> Bool {
+        return (x >= xPosition) && (x < xPosition + width)
+            && (y >= yPosition) && (y < yPosition + height)
+    }
+}
