7f9d14e32317e6e2bdae6c5cc1a2cf956579ea01
selemtei <selemtei@gmail.com>
2017-02-23 19:02:22 -0800

    initializeChange func

--- Warcraft2/BattleMode.swift
+++ Warcraft2/BattleMode.swift
@@ -5,4 +5,36 @@ class BattleMode: ApplicationMode{
         context->loadGameMap(context->selectedMapIndex)
         context->soundLibraryMixer->playSong(context->soundLibraryMixer->findSong("game1"), context->musicVolume)
     }
+    
+    func input(context:ApplicationData){
+        var currentX:Int
+        var currentY:Int
+        let panning = false
+        let shiftPressed = false
+        var panningDirection = Direction.max
+        
+        currentX = context->x
+        currentY = context->y
+        
+        context->GameModel->clearGameEvents()
+        for(key in context->pressedKeys){
+            if(GUIKeyType.upArrow == key){
+                panningDirection = Direction.north
+                panning = true
+            }else if(GUIKeyType.downArrow == key){
+                panningDirection = Direction.south
+                panning = true
+            }else if(GUIKeyType.leftArrow == key){
+                panningDirection = Direction.west
+                panning = true
+            }else if(GUIKeyType.leftShift == key || GUIKeyType.rightShift == key){
+                shiftPressed = true
+            }
+            
+        }
+        
+        
+        
+    }
+    
 }
