e45a3bee891e0a9aca5012af7e0550d12e3afa2b
kburckin <kburckin@gmail.com>
2017-03-05 22:36:06 -0800

    fixed food porduction and consumption in resource bar

--- Warcraft2/ResourceRenderer.swift
+++ Warcraft2/ResourceRenderer.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 
 enum MiniIconTypes: Int {
     case mitGold
@@ -24,12 +25,18 @@ func mitValue(mit: MiniIconTypes) -> Int {
 
 class ResourceRenderer {
     private(set) var player: PlayerData
+    private(set) var foregroundColor: UIColor
+    private(set) var backgroundColor: UIColor
+    private(set) var insufficientColor: UIColor
     private(set) var lastGoldDisplay: Int
     private(set) var lastLumberDisplay: Int
     private(set) var resourceBar: ResourceBarView
 
     init(loadedPlayer: PlayerData, resourceBarView: ResourceBarView) {
         player = loadedPlayer
+        foregroundColor = UIColor.white
+        backgroundColor = UIColor.black
+        insufficientColor = UIColor.red
         lastGoldDisplay = 0
         lastLumberDisplay = 0
         resourceBar = resourceBarView
@@ -38,19 +45,16 @@ class ResourceRenderer {
     func drawResources() {
         // animate gold update
         var deltaGold = player.gold - lastGoldDisplay
-
         deltaGold /= 5
         if -3 < deltaGold && 3 > deltaGold {
             lastGoldDisplay = player.gold
         } else {
             lastGoldDisplay += deltaGold
         }
-
         resourceBar.goldCount.text = String(lastGoldDisplay)
 
         // update lumber
         var deltaLumber = player.lumber - lastLumberDisplay
-
         deltaLumber /= 5
         if -3 < deltaLumber && 3 > deltaLumber {
             lastLumberDisplay = player.lumber
@@ -60,11 +64,13 @@ class ResourceRenderer {
         resourceBar.lumberCount.text = String(lastLumberDisplay)
 
         // update food
-
-        //        if player.foodConsumption > player.foodProduction {
-        //            // consumption/production
-        //        } else {
-        //            // print food consumption
-        //        }
+        if player.foodConsumption > player.foodProduction {
+            let foodInfo = NSMutableAttributedString(string: String(player.foodConsumption) + " / " + String(player.foodProduction))
+            let foodConsumptionStringLength = String(player.foodConsumption).characters.count
+            foodInfo.addAttribute(NSForegroundColorAttributeName, value: insufficientColor, range: NSRange(location: 0, length: foodConsumptionStringLength))
+            resourceBar.foodCount.attributedText = foodInfo
+        } else {
+            resourceBar.foodCount.text = String(player.foodConsumption) + " / " + String(player.foodProduction)
+        }
     }
 }
