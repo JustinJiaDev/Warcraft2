2315f3eab34cc9f1337e775e3bc4a6cc72d66915
Justin Jia <justin.jia.developer@gmail.com>
2017-03-08 15:50:18 -0800

    Clean up

--- Warcraft2/TrainCapabilities.swift
+++ Warcraft2/TrainCapabilities.swift
@@ -1,6 +1,6 @@
 struct TrainCapabilities {
     static let registrant = TrainCapabilities()
-    
+
     init() {
         PlayerCapability.register(capability: PlayerCapabilityTrainNormal(unitName: "Peasant"))
         PlayerCapability.register(capability: PlayerCapabilityTrainNormal(unitName: "Footman"))
@@ -14,7 +14,7 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
         private var totalSteps: Int
         private var lumber: Int
         private var gold: Int
-        
+
         init(actor: PlayerAsset, playerData: PlayerData, target: PlayerAsset, lumber: Int, gold: Int, steps: Int) {
             self.currentStep = 0
             self.totalSteps = steps
@@ -25,22 +25,22 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
             self.playerData.decrementGold(by: gold)
             self.target.pushCommand(AssetCommand(action: .construct, capability: .none, assetTarget: actor, activatedCapability: nil))
         }
-        
+
         override func percentComplete(max: Int) -> Int {
             return currentStep * max / totalSteps
         }
-        
+
         override func incrementStep() -> Bool {
             currentStep += 1
             actor.incrementStep()
             target.incrementStep()
-            
+
             target.incrementHitPoints((target.maxHitPoints * (currentStep + 1) / totalSteps) - (target.maxHitPoints * currentStep / totalSteps))
-            
+
             guard currentStep >= totalSteps else {
                 return false
             }
-            
+
             playerData.addGameEvent(GameEvent(type: .ready, asset: target))
             actor.popCommand()
             target.popCommand()
@@ -51,7 +51,7 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
             )
             return true
         }
-        
+
         override func cancel() {
             playerData.incrementLumber(by: lumber)
             playerData.incrementGold(by: gold)
@@ -59,15 +59,14 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
             actor.popCommand()
         }
     }
-    
+
     private var unitName: String
-    
-    
+
     init(unitName: String) {
         self.unitName = unitName
         super.init(name: "Build" + unitName, targetType: .none)
     }
-    
+
     override func canInitiate(actor: PlayerAsset, playerData: PlayerData) -> Bool {
         guard let assetType = playerData.assetTypes[unitName] else {
             return false
@@ -86,21 +85,20 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
         }
         return true
     }
-    
+
     override func canApply(actor: PlayerAsset, playerData: PlayerData, target: PlayerAsset) -> Bool {
         return canInitiate(actor: actor, playerData: playerData)
     }
-    
+
     override func applyCapability(actor: PlayerAsset, playerData: PlayerData, target: PlayerAsset) -> Bool {
         guard let assetType = playerData.assetTypes[unitName] else {
             return false
         }
-        
+
         let newAsset = playerData.createAsset(unitName)
         newAsset.tilePosition = Position.tile(fromAbsolute: actor.position)
         newAsset.hitPoints = 1
-        
-        
+
         let newCommand = AssetCommand(
             action: .capability,
             capability: assetCapabilityType,
