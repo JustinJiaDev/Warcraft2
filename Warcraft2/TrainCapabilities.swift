487f2f9f692932dda20537de915a950d7bae085f
annyhynna <annyhsu23@gmail.com>
2017-03-08 00:06:37 -0800

    Check TrainCapabilities

--- Warcraft2/TrainCapabilities.swift
+++ Warcraft2/TrainCapabilities.swift
@@ -1,12 +1,13 @@
 struct TrainCapabilities {
     static let registrant = TrainCapabilities()
-
+    
     init() {
         PlayerCapability.register(capability: PlayerCapabilityTrainNormal(unitName: "Peasant"))
         PlayerCapability.register(capability: PlayerCapabilityTrainNormal(unitName: "Footman"))
         PlayerCapability.register(capability: PlayerCapabilityTrainNormal(unitName: "Archer"))
     }
-
+    
+    // Does this need to be here since it's empty ?
     func register() {}
 }
 
@@ -16,7 +17,7 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
         private var totalSteps: Int
         private var lumber: Int
         private var gold: Int
-
+        
         init(actor: PlayerAsset, playerData: PlayerData, target: PlayerAsset, lumber: Int, gold: Int, steps: Int) {
             self.currentStep = 0
             self.totalSteps = steps
@@ -27,22 +28,22 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
             self.playerData.decrementGold(by: gold)
             self.target.pushCommand(AssetCommand(action: .construct, capability: .none, assetTarget: actor, activatedCapability: nil))
         }
-
+        
         override func percentComplete(max: Int) -> Int {
             return currentStep * max / totalSteps
         }
-
+        
         override func incrementStep() -> Bool {
             currentStep += 1
             actor.incrementStep()
             target.incrementStep()
-
+            
             target.incrementHitPoints((target.maxHitPoints * (currentStep + 1) / totalSteps) - (target.maxHitPoints * currentStep / totalSteps))
-
+            
             guard currentStep >= totalSteps else {
                 return false
             }
-
+            
             playerData.addGameEvent(GameEvent(type: .ready, asset: target))
             actor.popCommand()
             target.popCommand()
@@ -53,7 +54,7 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
             )
             return true
         }
-
+        
         override func cancel() {
             playerData.incrementLumber(by: lumber)
             playerData.incrementGold(by: gold)
@@ -61,14 +62,17 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
             actor.popCommand()
         }
     }
-
+    
     private var unitName: String
-
+    
+    
     init(unitName: String) {
         self.unitName = unitName
-        super.init(name: "Build", targetType: .none)
+        // C++ line 63: CPlayerCapabilityTrainNormal::CPlayerCapabilityTrainNormal(const std::string &unitname) :CPlayerCapability(std::string("Build") + unitname, ETargetType::ttNone)
+        // original Swift: super.init(name: "Build", targetType: .none)
+        super.init(name: "Build" + unitName, targetType: .none)
     }
-
+    
     override func canInitiate(actor: PlayerAsset, playerData: PlayerData) -> Bool {
         guard let assetType = playerData.assetTypes[unitName] else {
             return false
@@ -87,20 +91,21 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
         }
         return true
     }
-
+    
     override func canApply(actor: PlayerAsset, playerData: PlayerData, target: PlayerAsset) -> Bool {
         return canInitiate(actor: actor, playerData: playerData)
     }
-
+    
     override func applyCapability(actor: PlayerAsset, playerData: PlayerData, target: PlayerAsset) -> Bool {
         guard let assetType = playerData.assetTypes[unitName] else {
             return false
         }
-
+        
         let newAsset = playerData.createAsset(unitName)
         newAsset.tilePosition = Position.tile(fromAbsolute: actor.position)
         newAsset.hitPoints = 1
-
+        
+        
         let newCommand = AssetCommand(
             action: .capability,
             capability: assetCapabilityType,
@@ -111,11 +116,14 @@ class PlayerCapabilityTrainNormal: PlayerCapability {
                 target: newAsset,
                 lumber: assetType.lumberCost,
                 gold: assetType.goldCost,
-                steps: assetType.buildTime
+                // C++ line 108: CPlayerAsset::UpdateFrequency() * AssetType->BuildTime());
+                // original Swift: steps: assetType.buildTime
+                steps: PlayerAsset.updateFrequency * assetType.buildTime
             )
         )
         actor.pushCommand(newCommand)
         actor.resetStep()
+        // C++ line 112: original code always return false, not sure if that's a wrong implementation?
         return true
     }
 }
